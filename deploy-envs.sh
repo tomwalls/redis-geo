e7fe9054817dccbd13f328696d637ecbd35489de
tom.walls <tom.walls@scileads.com>
2019-01-23 21:31:56 +0000

    config

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -7,7 +7,7 @@ export IMAGE_VERSION=latest
 export AWS_DEFAULT_REGION=eu-west-1
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=clubrunner.xyz
-
+ 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 # optional: SERVICESTACK_LICENSE
